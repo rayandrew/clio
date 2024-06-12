c797424a94b017c1075245e9f522d5284bea8a86
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2024-06-12 16:31:36 +0000

    add pipeline script stub

---
+++ s/pipeline.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+labeling() {
+    local data
+    data=$(parse_opt_req "data:d" "$@")
+    log_info "Labeling the dataset: $data"
+}
+
+feature_engineering() {
+    log_info "Feature engineering"
+}
+
+chain() {
+    local data
+    data=$(parse_opt_req "data:d" "$@")
+    labeling --data="$data"
+    feature_engineering
+}
+
+# +=================+
+# |    __main__     |
+# +=================+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  [[ -z "${CLIO}" ]] && echo "CLIO env is not set" && exit 1
+  # shellcheck disable=SC1091
+  source "${CLIO}/util.sh"
+  dorun "$@"
+fi
